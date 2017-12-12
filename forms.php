5a1fa0e69d09ed33a6eeee2039a6aaf11b0d7220
halimahdiah <32287174+halimahdiah@users.noreply.github.com>
2017-12-12 12:58:53 +0800

    tambah

---
+++ forms.php
@@ -0,0 +1,143 @@
+﻿<?php
+$head = 'Form Pendaftaran';
+$header = 'Selamat Datang';
+require ('layout/header.php');
+//require ('layout/nav.php');
+?>
+   <!--  <section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="index.html">Dashboard</a></li>
+                            <li><a href="ui.html">UI Elements</a></li>
+                            <li><a href="table.html">Data Tables</a></li>
+                            <li><a class="menu-top-active"  href="forms.html">Forms</a></li>
+                            <li><a href="login.html">Login Page</a></li>
+                            <li><a href="blank.html">Blank Page</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section> -->
+    <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+        <div class="container">
+              <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-head-line">Form Pendaftaran Anggota</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                                    Email dan Password 
+                            </div>
+                            <div class="panel-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="email" class="form-control" placeholder="Enter Email Address" name="email" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input type="password" class="form-control" placeholder="Enter Password" name="password" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Konfirmasi Password</label>
+                                        <input type="password" class="form-control" placeholder="Enter Password Lagi" name="password" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                                    Data Diri Anda 
+                            </div>
+                            <div class="panel-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label>Nama Lengkap</label>
+                                        <input type="text" class="form-control" placeholder="Enter Nama Lengkap" name="nama" />
+                                    </div>
+                                    <div class="form-inline">
+                                        <label style="display: block">Jenis Kelamin</label>
+                                            <div class="row col-md-2">
+                                                <input type="radio" name="jk" id="optionsRadios2" value="laki-laki" checked />
+                                                Laki-laki
+                                            </div>
+                                            <!-- <div class="row col-md-2"> -->
+                                                <input type="radio" name="jk" id="optionsRadios2" value="perempuan" />
+                                                Perempuan
+                                            <!-- </div> -->
+                                    </div>
+                                    <div class="form-inline">
+                                        <label style="display: block">Tempat Tanggal Lahir</label>
+                                        <input type="text" class="form-control" name="tempat-lahir" placeholder="Tempat Lahir" />
+                                        <input type="date" class="form-control" name="tgl-lahir" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Alamat</label>
+                                        <input type="text" class="form-control" name="alamat" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>No. Telepon / Handphone</label>
+                                        <input type="text" class="form-control" name="telepon" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Pekerjaan</label>
+                                        <input type="text" class="form-control" name="pekerjaan" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Foto User</label>
+                                        <input type="file"/>
+                                    </div>
+                                    <!-- <button type="submit" class="btn btn-info">Submit</button> -->
+                                    <a href="login_anggota.php" class="btn btn-info">Submit</a>
+                                    
+
+                                    <!--  Modals-->
+                    
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                      Cancel
+                                    </button>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                      <div class="modal-dialog modal-sm" role="document">
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Konfirmasi</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                              <span aria-hidden="true">&times;</span>
+                                            </button>
+                                          </div>
+                                          <div class="modal-body">
+                                            Anda yakin ingin membatalkan pendaftaran?
+                                          </div>
+                                          <div class="modal-footer">
+                                            
+                                            <a href="login.php" class="btn btn-default">Yes</a>
+                                            <button type="button" class="btn btn-info" data-dismiss="modal">No</button>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>                     
+                                    <!-- End Modals-->                
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+        
+<?php require ('layout/footer.php'); ?>
---
+++ home.php
@@ -0,0 +1,145 @@
+<?php
+$head = 'Home';
+$header = 'Selamat Datang';
+require ('layout/header.php');
+//require ('layout/nav.php');
+?>
+
+<section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a class="menu-top-active" href="home.php">Home</a></li>
+                             
+                            <li><a data-toggle="dropdown" class="dropdown-toggle">Koleksi <span class="caret"></span></a>
+                            <!-- <li> -->
+                            <!-- <div class="btn-group"> -->
+											  <!-- <div class="btn btn-primary dropdown-toggle"><span class="caret"></span></div> -->
+											  
+											  <ul class="dropdown-menu">
+											  	<li><a href="#">Semua Koleksi</a></li>
+												<li><a href="#">Umum</a></li>
+												<li><a href="#">Filsafat</a></li>
+												<li><a href="#">Agama</a></li>
+												<li><a href="#">Sosial</a></li>
+												<li><a href="#">Bahasa</a></li>
+												<li><a href="#">Ilmu Murni</a></li>
+												<li><a href="#">Ilmu Terapan</a></li>
+												<li><a href="#">Kesenian</a></li>
+												<li><a href="#">Kesusasteraan</a></li>
+												<li><a href="#">Ilmu Bumi</a></li>
+											  </ul>
+											<!-- </div> -->
+							</li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+    <!-- MENU SECTION END-->
+
+<div class="content-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="page-head-line">Home</h1>
+            </div>
+        </div>
+        <div class="row">
+        <div class="col-xs-12">
+        	<div class="input-group input-group-lg">
+        		
+        		<input type="text" class="form-control" placeholder="Search for..." name="keyword"/>
+	        	<span class="input-group-btn">
+	        	<button type="button" class="btn btn-info"><span class="glyphicon glyphicon-search"></span> Search</button>
+	        	</span>
+        	</div>
+        	</div>
+        </div>
+        <hr>
+        <div class="row">
+                <div class="col-md-4 col-sm-4">
+                    <div class="panel panel-success">
+                        
+                        <div class="panel-body" align="center">
+                        	<img src="http://localhost/rplkelompok5/image/images_001.jpg" width="50%">
+                            <h4>Basis Data Revisi Kedua</h4>
+                            <p><i>Fathansyah</i></p>
+                            <a href="" class="btn btn-success">Detail</a>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="panel panel-success">
+                        
+                        <div class="panel-body" align="center">
+                        	<img src="http://localhost/rplkelompok5/image/images_002.jpg" width="50%">
+                            <h4>Bintang</h4>
+                            <p><i>Tere Liye</i></p>
+                            <a href="" class="btn btn-success">Detail</a>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="panel panel-success">
+                        
+                        <div class="panel-body" align="center">
+                        	<img src="http://localhost/rplkelompok5/image/images_003.jpg" width="50%">
+                            <h4>Ilmu Bahan Bangunan</h4>
+                            <p><i>Dwi Yanthi Winoto</i></p>
+                            <a href="" class="btn btn-success">Detail</a>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+                <div class="row">
+                <div class="col-md-4 col-sm-4">
+                    <div class="panel panel-success">
+                        
+                        <div class="panel-body" align="center">
+                        	<img src="http://localhost/rplkelompok5/image/images_004.jpg" width="50%">
+                            <h4>Jalan Jalan: A Novel of Indonesia</h4>
+                            <p><i>Mike Stoner</i></p>
+                            <a href="" class="btn btn-success">Detail</a>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="panel panel-success">
+                        
+                        <div class="panel-body" align="center">
+                        	<img src="http://localhost/rplkelompok5/image/images_005.jpg" width="50%">
+                            <h4>The Power of Digital Marketing</h4>
+                            <p><i>Gm Susanto</i></p>
+                            <a href="" class="btn btn-success">Detail</a>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="panel panel-success">
+                        
+                        <div class="panel-body" align="center">
+                        	<img src="http://localhost/rplkelompok5/image/images_006.jpg" width="50%">
+                            <h4>Keperawatan Medikal Bedah: Sistem Kardiovaskular</h4>
+                            <p><i>M. Asikin - M. Nuralamsyah - Susaldi</i></p>
+                            <a href="" class="btn btn-success">Detail</a>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+
+
+
+<?php require ('layout/footer.php'); ?>
