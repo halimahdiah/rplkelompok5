07abe69c14c825eeb0f1e9ac23651ee732ca2caa
FernandyDito <32442987+FernandyDito@users.noreply.github.com>
2018-01-15 17:32:57 +0800

    fix
    sip

--- keanggotaan/print.php
+++ keanggotaan/print.php
@@ -6,60 +6,56 @@ $pdf = new FPDF('L','mm','A3');
 // membuat halaman baru
 $pdf->AddPage();
 // setting jenis font yang akan digunakan
-$pdf->SetFont('Arial','B',16);
+$pdf->SetFont('Arial','B',21);
 // mencetak string 
 
-$pdf->Cell(0,20, "LAPORAN DATA BUKU", 0, 1, 'C');
+$pdf->Cell(0,20, "LAPORAN ANGGOTA PERPUSTAKAAN", 0, 1, 'C');
 
 // Memberikan space kebawah agar tidak terlalu rapat
 $pdf->Cell(10,7,'',0,1);
 
-$pdf->SetFont('Arial','B',10);
+$pdf->SetFont('Arial','B',14);
 $pdf->SetFillColor(255,0,0);
 $pdf->SetTextColor(255);
 $pdf->SetDrawColor(128,0,0);
 
 // $pdf->Cell($kolom['length'], 5, $kolom['label'], 1, '0', $kolom['align'], true);
-$pdf->Cell(7,5,'ID',1,0,"L", true);
-$pdf->Cell(112,5,'Judul',1,0,"L", true);
-$pdf->Cell(49,5,'Pengarang',1,0,"L", true);
-$pdf->Cell(55,5,'Penerbit',1,0,"L", true);
-$pdf->Cell(15,5,'Tahun',1,0,"L", true);
-$pdf->Cell(25,5,'Tipe Buku',1,0,"L", true);
-$pdf->Cell(17,5,'Halaman',1,0,"L", true);
-$pdf->Cell(28,5,'ISBN',1,0,"L", true);
-$pdf->Cell(28,5,'Jumlah Buku',1,0,"L", true);
-$pdf->Cell(20,5,'Status',1,0,"L", true);
-$pdf->Cell(40,5,'Update Terakhir',1,1,"L", true);
+$pdf->Cell(30,7,'ID',1,0,"L", true);
+$pdf->Cell(78,7,'Nama',1,0,"L", true);
+$pdf->Cell(38,7,'Jenis Kelamin',1,0,"L", true);
+$pdf->Cell(35,7,'Tempat Lahir',1,0,"L", true);
+$pdf->Cell(35,7,'Tanggal Lahir',1,0,"L", true);
+$pdf->Cell(40,7,'Alamat',1,0,"L", true);
+$pdf->Cell(40,7,'Nomor Telepon',1,0,"L", true);
+$pdf->Cell(35,7,'Pekerjaan',1,0,"L", true);
+$pdf->Cell(48,7,'Email',1,1,"L", true);
 
 // $pdf->Ln();
 
-$pdf->SetFont('Arial','',10);
+$pdf->SetFont('Arial','',12);
 $pdf->SetFillColor(224,235,255);
 $pdf->SetTextColor(0);
 
 $host       = "localhost";
 $user       = "root";
-$password   = "1998";
+$password   = "fernandy21";
 $database   = "siperpustakaan";
 $connect    = mysqli_connect($host, $user, $password, $database);
 
-$query = "SELECT * FROM book";
+$query = "SELECT * FROM anggota";
 $sql = mysqli_query ($connect, $query);
 $data = array();
 while ($row = mysqli_fetch_assoc($sql)) {
-    $pdf->Cell(7,5,$row['Book_id'],1,0,"L", false);
-    $pdf->Cell(112,5,$row['Title'],1,0,"L", false);
-    $pdf->Cell(49,5,$row['Author'],1,0,"L", false);
-    $pdf->Cell(55,5,$row['Publisher'],1,0,"L", false);
-    $pdf->Cell(15,5,$row['Tahun_terbit'],1,0,"L", false);
-    $pdf->Cell(25,5,$row['Type'],1,0,"L", false);
-    $pdf->Cell(17,5,$row['Total_page'],1,0,"L", false);
-    $pdf->Cell(28,5,$row['ISBN'],1,0,"L", false);
-    $pdf->Cell(28,5,$row['Jumlah_copy'],1,0,"L", false);
-    $pdf->Cell(20,5,$row['Status'],1,0,"L", false);
-    $pdf->Cell(40,5,$row['Last_update'],1,1,"L", false);
-    
+    $pdf->Cell(30,7,$row['username'],1,0,"L", false);
+    $pdf->Cell(78,7,$row['nama'],1,0,"L", false);
+    $pdf->Cell(38,7,$row['jk'],1,0,"L", false);
+    $pdf->Cell(35,7,$row['tempat_lahir'],1,0,"L", false);
+    $pdf->Cell(35,7,$row['tanggal_lahir'],1,0,"L", false);
+    $pdf->Cell(40,7,$row['alamat'],1,0,"L", false);
+    $pdf->Cell(40,7,$row['telepon'],1,0,"L", false);
+    $pdf->Cell(35,7,$row['pekerjaan'],1,0,"L", false);
+    $pdf->Cell(48,7,$row['email'],1,1,"L", false);
+
 }
 
 
